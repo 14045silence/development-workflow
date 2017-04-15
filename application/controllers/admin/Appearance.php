cadbed16f1ab8a5b3c39d026a5d9a0354bdfa8e1
fatchul <ftchulamin@gmail.com>
2017-04-15 15:16:02 +0700

    edit index function

--- application/controllers/admin/Appearance.php
+++ application/controllers/admin/Appearance.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Appearance extends CI_Controller {
+class Appearancse extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
