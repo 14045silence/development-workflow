7fb8019b2487257d296456ee01d9ab2a549469c3
fatchul <ftchulamin@gmail.com>
2017-04-15 15:17:10 +0700

    Update Appearance.php

--- application/controllers/admin/Appearance.php
+++ application/controllers/admin/Appearance.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Appearancse extends CI_Controller {
+class Appearance extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
