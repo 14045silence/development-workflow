5d72bc63ddbcf7d81120fcc88b12ec03a15cda17
fatchul <ftchulamin@gmail.com>
2017-04-17 23:28:01 +0700

    create 2 tables

--- application/migrations/20170417181756_first_migrate.php
+++ application/migrations/20170417181756_first_migrate.php
@@ -5,6 +5,7 @@ class Migration_First_migrate extends CI_Migration
 	
 	function up()
 	{
+		//blog
 		$this->dbforge->add_field(array(
 			'blog_id' => array(
 				'type' => 'INT',
@@ -23,6 +24,27 @@ class Migration_First_migrate extends CI_Migration
 			));
 		$this->dbforge->add_key('blog_id', TRUE);
 		$this->dbforge->create_table('blog');
+
+
+		// contoh
+		$this->dbforge->add_field(array(
+			'blog_id' => array(
+				'type' => 'INT',
+				'constraint' => 5,
+				'unsigned' => TRUE,
+				'auto_increment' => TRUE
+				),
+			'blog_title' => array(
+				'type' => 'VARCHAR',
+				'constraint' => '100',
+				),
+			'blog_description' => array(
+				'type' => 'TEXT',
+				'null' => TRUE,
+				),
+			));
+		$this->dbforge->add_key('blog_id', TRUE);
+		$this->dbforge->create_table('contoh');
 	}
 	function down()
 	{
